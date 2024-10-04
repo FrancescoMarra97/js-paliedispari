91bfb1e8366f70a6508f163c4050c9e127f2818e
FrancescoMarra97 <francescomarra192@gmail.com>
2024-10-04 16:14:34 +0200

    1st function

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -6,18 +6,20 @@ se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
  */
 
-// vuole due funzioni , dividiamo il lavoro. Prima funzione 1
+// vuole due funzioni , dividiamo il lavoro.
+// Prima funzione 1
+function getUserInputAndGenerateRandom(){
+    
 
 //l'utente sceglie pari o dispari e inserisce un numero
-
-
+let message=prompt("Scegli tra pari o dispari");
+let num1=Number(prompt("inserisci un numero tra 1 e 5"));
 
 //facciamo generare un numero random dal pc
+let num2=Math.floor(Math.random() * 5) + 1;
 
 
-
-// fine prima funzione
-
+}// fine prima funzione
 
 
 //seconda funzione 
