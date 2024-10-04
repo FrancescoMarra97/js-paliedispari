c7f41c4e0a407a574abc33d50ab52280d82f4c89
FrancescoMarra97 <francescomarra192@gmail.com>
2024-10-04 15:38:17 +0200

    ex 1

--- palindromi/mian.js
+++ palindromi/mian.js
@@ -1,3 +1,6 @@
+/* Chiedere all’utente di inserire una parola 
+Creare una funzione per capire se la parola inserita è palindroma */
+
 function isPalindrome(inputWord) {
     
     //valore da restituire
@@ -7,8 +10,14 @@ function isPalindrome(inputWord) {
     let reversedWord=inputWord.split("").reverse().join("")
 
     //controllare che le due parole siano uguali
-    if (inputWord !== resversedWord) {
+    if (inputWord !== reversedWord) {
         validate=false
     }
     return validate;
 }
+//chiedere all'utente
+let message=prompt("Inserisci una parola");
+let result=isPalindrome(message);
+console.log(result);
+
+
