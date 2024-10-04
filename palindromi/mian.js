b5e1afd0206395a347c3adab6c7db6eae5f81ce6
FrancescoMarra97 <francescomarra192@gmail.com>
2024-10-04 15:26:12 +0200

    invertire la stringa

--- palindromi/mian.js
+++ palindromi/mian.js
@@ -1,3 +1,14 @@
-function isPalindrome() {
+function isPalindrome(inputWord) {
     
+    //valore da restituire
+    let validate= true;
+
+    //invertire la parola
+    let reversedWord=inputWord.split("").reverse().join("")
+
+    //controllare che le due parole siano uguali
+    if (inputWord !== resversedWord) {
+        validate=false
+    }
+    return validate;
 }
